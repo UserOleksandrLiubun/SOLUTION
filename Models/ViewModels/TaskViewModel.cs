9bf2d5fc2b7588bccc89f206b4303ec8069837d4
Oleksandr Liubun <oleksandrliubun@gmail.com>
2025-11-23 20:36:45 +0200

    -

--- Models/ViewModels/TaskViewModel.cs
+++ Models/ViewModels/TaskViewModel.cs
@@ -6,12 +6,12 @@ public class TaskViewModel
     public int Id { get; set; }
     public string? UserId { get; set; }
     public string? EditorId { get; set; }
-    [Display(Name = "Due Date")]
+    [Display(Name = "Виконати до")]
     public DateTime? DueDate { get; set; }
     [Required]
     public string Name { get; set; }
-    [Display(Name = "Description")]
-    [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
+    [Display(Name = "Опис")]
+    [StringLength(1000, ErrorMessage = "Опис не може перевищувати 1000 символів")]
     [DataType(DataType.MultilineText)]
     public string Description { get; set; }
     [Display(Name = "Is Important")]
